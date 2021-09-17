ef41a01302d1391cbb6007fdbd7b0fa5dfcf2c40
praveena <73698635+Praveena6@users.noreply.github.com>
2021-09-17 18:17:02 +0530

    Add files via upload

---
+++ bank.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 17, 2021 at 02:45 PM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bank`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `name` varchar(20) NOT NULL,
+  `emailid` varchar(20) NOT NULL,
+  `balance` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`name`, `emailid`, `balance`) VALUES
+('Abi', 'abi123@gmail.com', 35000),
+('Bala', 'bala123@gmail.com', 15000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transfer`
+--
+
+CREATE TABLE `transfer` (
+  `name` varchar(20) DEFAULT NULL,
+  `tname` varchar(20) DEFAULT NULL,
+  `amount` int(40) DEFAULT NULL,
+  `balance` int(40) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `transfer`
+--
+
+INSERT INTO `transfer` (`name`, `tname`, `amount`, `balance`) VALUES
+('Bala', 'Abi', 3000, 15000),
+('Abi', 'Bala', 3000, 30000),
+('Bala', 'Abi', 10000, 10000),
+('Abi', 'Bala', 5000, 35000);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
